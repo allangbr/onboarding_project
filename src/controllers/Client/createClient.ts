33f4c477ed7e2f7e090e1064031d6237a63eeea9
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 14:19:04 -0300

    Implementando o controller do método createClient #3

---
+++ src/controllers/Client/createClient.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { prisma } from "../../service/prisma";
+import { CreateClient } from "../../usecases/Client/createClient";
+var HTTPStatusCode = require('http-status-code');
+
+export class CreateClientController{
+  async handle(req: Request, res: Response){
+    const {username, password, name, email, number, address} = req.body;
+
+    try{
+      if(await prisma.client.findUnique({
+        where: {
+          username: username
+        }
+      })){
+        return res.status(HTTPStatusCode.getMessage(401)).send({error: "Cliente Já Existente"})
+      }
+
+      const create = new CreateClient();
+
+      //Criando Client
+      const client = await create.execute({username, password, name, email, number, address});
+
+      //Retornando Membro Cadastrado
+      return res.send({
+        client,
+      },);
+    } catch (err){
+      return res.status(HTTPStatusCode.getMessage(404)).send({error: "Falha no Registro do Cliente"});
+    }
+  }
+}
