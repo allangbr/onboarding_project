110e5437beaef3b7b6ec9deefbe4cc51ef135327
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 19:12:17 -0300

    feat/adicionando rota da funcao updateClient (#3)

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+
+//Importando os Controlllers das funções
 import { CreateClientController } from "../controllers/Client/createClient";
 import { GetAllClientsController } from "../controllers/Client/getAllClients";
 import { GetClientController } from "../controllers/Client/getClient";
+import { UpdateClientController } from "../controllers/Client/updateClient"; 
 
 export const clientRoutes = Router();
 
@@ -14,12 +17,15 @@ const getAllClientsController = new GetAllClientsController();
 //Instânciando o Controller do método GetClient
 const getClientController = new GetClientController();
 
+//Instânciando o Controller do método UpdateClient
+const updateClientController = new UpdateClientController();
+
 clientRoutes.post("/create", createClientController.handle);
 
 clientRoutes.get("/getAll", getAllClientsController.handle);
 
 clientRoutes.get("/:username", getClientController.handle);
 
-clientRoutes.put("/:id", );
+clientRoutes.put("/update", updateClientController.handle);
 
 clientRoutes.delete("/:username", );
