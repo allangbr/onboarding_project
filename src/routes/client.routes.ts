af92a8a9391807d477d8632f6447c9ea3ed7fdbc
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 13:58:00 -0300

    Implementando rotas do Client #3

---
+++ src/routes/client.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+export const clientRoutes = Router();
+
+clientRoutes.post("/create", );
+
+clientRoutes.get("/getAll", );
+
+clientRoutes.get("/:username", );
+
+clientRoutes.put("/:id", );
+
+clientRoutes.delete("/:username", );
