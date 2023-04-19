b67bd3d8fd5a827a427023b42ec6224f7431d6de
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 18:22:27 -0300

    Adicionando a rota da função getAllClients (#3)

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { CreateClientController } from "../controllers/Client/createClient";
+import { GetAllClientController } from "../controllers/Client/getAllClients";
 
 export const clientRoutes = Router();
 
 //Instânciando o Controller do método CreateClient
 const createClientController = new CreateClientController();
 
+//Instânciando o Controller do método GetAllClients
+const getAllClientController = new GetAllClientController();
+
 clientRoutes.post("/create", createClientController.handle);
 
-clientRoutes.get("/getAll", );
+clientRoutes.get("/getAll", getAllClientController.handle);
 
 clientRoutes.get("/:username", );
 
