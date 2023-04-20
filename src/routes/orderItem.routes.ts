a7c41527034c36e0173c6bb12b846fd40bd9b0db
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 14:45:55 -0300

    feat/adicionando rota da funcao getOrderItem (#10)

--- src/routes/orderItem.routes.ts
+++ src/routes/orderItem.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 //Importando os Controllers das Funções
 import { CreateOrderItemController } from "../controllers/OrderItem/createOrderItem";
 import { GetAllOrderItemsController } from "../controllers/OrderItem/getAllOrderItems";
+import { GetOrderItemController } from "../controllers/OrderItem/getOrderItem";
 
 export const orderItemRoutes = Router();
 
@@ -12,12 +13,16 @@ const createOrderItemController = new CreateOrderItemController();
 //Instânciando o Controller do método GetAllOrderItems
 const getAllOrderItemsController = new GetAllOrderItemsController();
 
+//Instânciando o Controller do método GetOrderItem
+const getOrderItemController = new GetOrderItemController();
+
+
 //Estabelecendo as rotas
 orderItemRoutes.post("/create", createOrderItemController.handle);
 
 orderItemRoutes.get("/getAll", getAllOrderItemsController.handle);
 
-orderItemRoutes.get("/:id", );
+orderItemRoutes.get("/:id", getOrderItemController.handle);
 
 orderItemRoutes.put("/:id", );
 
