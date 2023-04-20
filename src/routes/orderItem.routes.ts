976298213bf7316c268184d087338c0879f746b0
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 14:18:18 -0300

    feat/implementando rotas gerais do orderItem (#10)

---
+++ src/routes/orderItem.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+export const orderItemRoutes = Router();
+
+//Estabelecendo as rotas
+orderItemRoutes.post("/create", );
+
+orderItemRoutes.get("/getAll", );
+
+orderItemRoutes.get("/:id", );
+
+orderItemRoutes.put("/:id", );
+
+orderItemRoutes.delete("/:id", );
