58ace96029e71a9745f4f4b92340ee750c617cd2
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 22:33:56 -0300

    feat/implementando controller deleteOrder (#5)

---
+++ src/controllers/Order/deleteOrder.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { prisma } from "../../service/prisma";
+import { DeleteOrder } from "../../usecases/Order/deleteOrder";
+import { StatusCodes } from "http-status-codes";
+
+export class DeleteOrderController{
+  async handle(req: Request, res: Response){
+    const { id } = req.params;
+    try{
+      //Verificando se o pedido informado existe
+      const testOrder = await prisma.order.findUnique({
+        where: {
+          id: Number(id)
+        }
+      })
+      if(testOrder == null){
+        return res.status(StatusCodes.UNAUTHORIZED).send({error: "O Pedido informado não existe"})
+      }
+
+      const deleteOrder = new DeleteOrder();
+
+      //Deletando o Pedido
+      const deletedOrder = await deleteOrder.execute(Number(id));
+
+      //Retornando o pedido
+      return res.status(StatusCodes.OK).send({
+        deletedOrder,
+      },);
+    } catch (err){
+      return res.status(StatusCodes.BAD_REQUEST).send({error: "Falha na busca pelo Pedido"});
+    }
+  }
+}
