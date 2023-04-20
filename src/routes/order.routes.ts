f02b8e01bc47eae2768708e67c0701e2afa15f81
allangbr <allanalmeidagbr@gmail.com>
2023-04-19 21:57:50 -0300

    feat/Adicionando a rota da funcao getAllOrders (#5)

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -2,16 +2,20 @@ import { Router } from "express";
 
 //Importando os Controllers das Funções
 import { CreateOrderController } from "../controllers/Order/CreateOrder";
+import { GetAllOrdersController } from "../controllers/Order/getAllOrders";
 
 export const orderRoutes = Router();
 
 //Instânciando o Controller do método CreateOrder
 const createOrderController = new CreateOrderController();
 
+//Instânciando o Controller do método GetAllOrders
+const getAllOrdersController = new GetAllOrdersController();
+
 //Estabelecendo as rotas
 orderRoutes.post("/create", createOrderController.handle);
 
-orderRoutes.get("/getAll", );
+orderRoutes.get("/getAll", getAllOrdersController.handle);
 
 orderRoutes.get("/:id", );
 
