f5b208e310a94a4249322b8e8ac86204a444f6e9
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 10:13:46 -0300

    feat/adicionando rota da funcao createMenuItem (#7)

--- src/routes/menuItem.routes.ts
+++ src/routes/menuItem.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+//Importando os Controllers das Funções
+import { CreateMenuItemController } from "../controllers/MenuItem/createMenuItem";
+
 export const menuItemRoutes = Router();
 
+//Instânciando o Controller do método CreateMenuItem
+const createMenuItemController = new CreateMenuItemController();
+
 //Estabelecendo as rotas
-menuItemRoutes.post("/create", );
+menuItemRoutes.post("/create", createMenuItemController.handle);
 
 menuItemRoutes.get("/getAll", );
 
