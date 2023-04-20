f65a21119abcd68bcedc24afb0b8fc1b26ed6d67
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 10:25:49 -0300

    feat/adicionando rota da funcao getMenuItem (#7)

--- src/routes/menuItem.routes.ts
+++ src/routes/menuItem.routes.ts
@@ -3,21 +3,25 @@ import { Router } from "express";
 //Importando os Controllers das Funções
 import { CreateMenuItemController } from "../controllers/MenuItem/createMenuItem";
 import { GetAllMenuItemsController } from "../controllers/MenuItem/getAllMenuItems";
+import { GetMenuItemController } from "../controllers/MenuItem/getMenuItem";
 
 export const menuItemRoutes = Router();
 
 //Instânciando o Controller do método CreateMenuItem
 const createMenuItemController = new CreateMenuItemController();
 
-//Instânciando o Controller do método GetAllMenuItem
+//Instânciando o Controller do método GetAllMenuItems
 const getAllMenuItemsController = new GetAllMenuItemsController();
+
+//Instânciando o Controller do método GetMenuItem
+const getMenuItemController = new GetMenuItemController();
 
 //Estabelecendo as rotas
 menuItemRoutes.post("/create", createMenuItemController.handle);
 
 menuItemRoutes.get("/getAll", getAllMenuItemsController.handle);
 
-menuItemRoutes.get("/:id", );
+menuItemRoutes.get("/:id", getMenuItemController.handle);
 
 menuItemRoutes.put("/:id", );
 
