22574ad12f5f5d9ab48391e54456605789b46a02
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 10:00:04 -0300

    feat/implementando rotas gerais do menuItem (#7)

---
+++ src/routes/menuItem.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+export const menuItemRoutes = Router();
+
+//Estabelecendo as rotas
+menuItemRoutes.post("/create", );
+
+menuItemRoutes.get("/getAll", );
+
+menuItemRoutes.get("/:id", );
+
+menuItemRoutes.put("/:id", );
+
+menuItemRoutes.delete("/:id", );
