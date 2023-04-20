eb33a249621fd26e76f5d5539133b593d79b4b44
allangbr <allanalmeidagbr@gmail.com>
2023-04-20 14:35:24 -0300

    feat/adicionando rota da funcao createOrderItem (#10)

--- src/routes/orderItem.routes.ts
+++ src/routes/orderItem.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 
+//Importando os Controllers das Funções
+import { CreateOrderItemController } from "../controllers/OrderItem/createOrderItem";
+
 export const orderItemRoutes = Router();
 
+//Instânciando o Controller do método CreateOrder
+const createOrderItemController = new CreateOrderItemController();
+
 //Estabelecendo as rotas
-orderItemRoutes.post("/create", );
+orderItemRoutes.post("/create", createOrderItemController.handle);
 
 orderItemRoutes.get("/getAll", );
 
